a3fdac00b0c7d8ad5902f4f57db0aead017f569b
xyerakovalskia <xyera123987@gmail.com>
2026-03-03 11:45:54 +0700

    BubbleSort5

---
+++ BubbleSort5.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];   
+int n;         
+
+void input(){ 
+    int d;
+
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array : "; 
+        cin >> n;                                         
+        if (n <= 20)      
+            break;        
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; 
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)  
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1; 
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        { 
+            if (arr[j] > arr[j + 1])
+            { 
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass += 1; 
+
+        cout << "\nPass " << pass - 1 << ": "; 
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; 
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); 
+}
+
+void display()
+{
+    cout << endl;
+    cout << "==============================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "==============================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j]; 
+        if (j < n - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl; 
+    cout << endl;
+    cout << endl;
+}
+
+int main()
+{
+
+    input();
+
+    bubbleSortArray();
+    display();
+
+    system("pause");
+    return 0;
+}
