dff3fb73fa88442e03da08f6411d9de118b9e53b
xyerakovalskia <xyera123987@gmail.com>
2026-03-03 11:40:35 +0700

    BubbleSort2

---
+++ BubbleSort2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];   
+int n;         
+
+void input(){ 
+    int d;
+
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array : "; 
+        cin >> n;                                         
+        if (n <= 20)      
+            break;        
+        else
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; 
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < n; i++)  
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
